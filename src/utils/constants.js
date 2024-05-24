b9248ad6e66ffd7c21775db6ec6513c0772cd2da
Roopesh-N <roopeshnagineni123@outlook.com>
2024-05-24 19:57:02 -0400

    secured keys

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 export const hamburger="https://static.vecteezy.com/system/resources/previews/002/292/406/original/hamburger-menu-line-icon-free-vector.jpg";
 export const youtubeLogo="https://lh3.googleusercontent.com/3zkP2SYe7yYoKKe47bsNe44yTgb4Ukh__rBbwXwgkjNRe4PykGG409ozBxzxkrubV7zHKjfxq6y9ShogWtMBMPyB3jiNps91LoNH8A=s500";
 export const userlogo="https://media.istockphoto.com/id/1337144146/vector/default-avatar-profile-icon-vector.jpg?s=612x612&w=0&k=20&c=BIbFwuv7FxTWvh5S3vB6bkT0Qv8Vn8N5Ffseq84ClGI=";
-const GOOGLE_API_KEY="AIzaSyAMvwB8_7CPBX3ch9YuYNh82BCfjTg9uus";
+const GOOGLE_API_KEY=process.env.REACT_APP_GOOGLE_API_KEY
 
 export const HOMEPAGE_VIDEOS_API='https://youtube.googleapis.com/youtube/v3/videos?part=snippet&part=statistics&chart=mostPopular&maxResults=40&regionCode=IN&key='+GOOGLE_API_KEY;
 
